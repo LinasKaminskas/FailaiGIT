c3e7643380109dfc3508449fcba0124c063b42d7
DESKTOP-1TDGMUF\Nemesis <linaskamin@gmail.com>
2018-10-11 15:13:32 +0300

    keitimas

--- biolatte_mockup/header.php
+++ biolatte_mockup/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Pavyzdys</title>
+    <title>Biolatte Mockup</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
@@ -15,14 +15,18 @@
 <body>
 <header>
         <div class="header-pagalbinis">
+            <img class="img-wrapper" src="../biolatte_mockup/img/slider.png">
+            <div class="Shape_5"></div>
+            <div class="taust_1"></div>
+
             <div class="container">
                 <div class="row">
                     <div class="col-md-12">
                         <div class="header-content-wrapper">
 
-                                <img class="img-wrapper" src="../biolatte_mockup/img/slider.png">
 
-                    </div>
+
+                        </div>
                 </div>
             </div>
         </div>
