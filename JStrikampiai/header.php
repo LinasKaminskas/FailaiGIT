d62a1e03a661c899a26ad23f93aa3eae6d5e2da4
DESKTOP-1TDGMUF\Nemesis <linaskamin@gmail.com>
2018-10-15 18:41:36 +0300

    Keitimas

--- JStrikampiai/header.php
+++ JStrikampiai/header.php
@@ -5,11 +5,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Pavyzdys</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <link rel="stylesheet" type="text/css" media="screen" href="../JStrikampiai/css/main.css"/>
 </head>
 <body>
@@ -20,7 +15,15 @@
                     <div class="col-md-12">
                         <div class="header-content-wrapper">
 
-                            <h1 Id="trikampis"></h1>
+                            <div Id="trikampis"></div>
+
+                            <table id="myTable1">
+                                <tr>
+                                    <td>Pirmas kampainis</td>
+                                </tr>
+                            </table>
+
+
 
                         </div>
                     </div>
@@ -28,6 +31,16 @@
             </div>
         </div>
 </header>
+<div class="container">
+    <div class="row_copy">
+        <div class="1class"></div>
+        <div class="2class"></div>
+        <div class="3class"></div>
+        <div class="row_paste">
+
+        </div>
+    </div>
+</div>
 <script src="../JStrikampiai/js/main.js"></script>
 
 
--- JStrikampiai/js/main.js
+++ JStrikampiai/js/main.js
@@ -1,28 +1,48 @@
-var a = 5;
-var b = 5;
-var c = 5;
-if(a+b>c&&b+c>a&&a+c>b){
-    if(a==b==c){
-        var plotas=a+b+c/3;
-        var ats1=document.getElementById("trikampis");
-        ats1.innerHTML+='Trikampis lygiakrastis'+plotas;
-    }
-    if(a==b&&a!=c&&b!=c||b==c&&a!=b&&a!=c||a==c&&a!=b&&c!=b){
-        var plotas=a+b+c/3;
-        var ats2=document.getElementById("trikampis");
-        ats2=innerHTML+='Trikampis lygiasonis'+plotas;
-    }
-    /*if(a!=b!=c){
-        var plotas=a+b+c/3;
-        var ats3=document.getElementById("trikampis");
-        ats3=innerHTML+='Paprastas trikampis'+plotas;
-}*/
-    else{
-        console.log('Ne trikampis');
-        var ats_ne=document.getElementById("trikampis");
-        ats_ne=innerHTML+='Ne trikampis';
 
+var a = 3;
+var  b = 4;
+var c = 5;
+var perimeter = (a + b + c) / 2;
+var area = Math.sqrt(perimeter*((perimeter - a)*(perimeter-b)*(perimeter-c)));
+
+if (a < b + c && b < a + c && c < a + b) {
+    console.log('Galime');
+    document.getElementById("trikampis").innerHTML='Galimas trikampis';
+    if (a == b && b == c ) {
+        console.log('lygiakrastis');
+        document.getElementById("trikampis").innerHTML='Trikampis lygiakrastis ' + area.toFixed(2) + ' plotas';
+    } else if  (a == b && a != c || a == c && a != b || b == c && b != a) {
+        console.log('lygiasonis');
+        document.getElementById("trikampis").innerHTML='Trikampis lygiasonis '+ area.toFixed(2) + ' plotas';
+    } else {
+        console.log('ivairiakrastis');
+        document.getElementById("trikampis").innerHTML='Trikampis ivairiasonis '+ area.toFixed(2) + ' plotas';
     }
 }
+else {
+    console.log('negalime');
+}
 
-/*document.getElementById("trikampis").innerHTML="Galimas";*/
+//masyvas
+
+/*var m  = [[15, 10 ,15, 20], [20, 10 ,20, 10], [7, 7 ,7, 7]];
+var  a = m[0];
+var b = m[1];
+var c = m[2];*/
+
+var m = [ 22, 64, 0, -55, 4, 7, -1, 8, -66 ];
+var  viso = 0;
+var i = 0;
+while (i<m.length){
+    viso += m[i];
+    i++;
+}
+
+console.log(viso/m.length);
+
+var i = 0;
+for (i > 0; i< 21;i+=4){
+    console.log(i);
+}
+
+
