3767e4be77adde7db5edc6d04064d38a12eeca2b
LinasKaminskas <linas32147>
2018-10-16 07:11:55 +0300

    7:11 2018-10-16

--- JStrikampiai/js/main.js
+++ JStrikampiai/js/main.js
@@ -8,7 +8,7 @@ var area = Math.sqrt(perimeter*((perimeter - a)*(perimeter-b)*(perimeter-c)));
 if (a < b + c && b < a + c && c < a + b) {
     console.log('Galime');
     document.getElementById("trikampis").innerHTML='Galimas trikampis';
-    if (a == b && b == c ) {
+    if (b == a && b == c) {
         console.log('lygiakrastis');
         document.getElementById("trikampis").innerHTML='Trikampis lygiakrastis ' + area.toFixed(2) + ' plotas';
     } else if  (a == b && a != c || a == c && a != b || b == c && b != a) {
@@ -30,19 +30,58 @@ var  a = m[0];
 var b = m[1];
 var c = m[2];*/
 
-var m = [ 22, 64, 0, -55, 4, 7, -1, 8, -66 ];
+/*var m = [ 22, 64, 0, -55, 4, 7, -1, 8, -66 ];
 var  viso = 0;
 var i = 0;
 while (i<m.length){
     viso += m[i];
     i++;
-}
+}*/
 
-console.log(viso/m.length);
+//console.log(viso/m.length);
 
-var i = 0;
+/*var i = 0;
 for (i > 0; i< 21;i+=4){
     console.log(i);
-}
+}*/
+
+//17.1
+
+/*var counter = -10;
+while (counter < 20) {
+    console.log(counter);
+    counter++;
+}*/
+
+//17.2
+
+/*var  counter = 10;
+while (counter <= 40) {
+    console.log(counter);
+    counter += 2;
+}*/
+
+//17.3
+
+/*var counter = 300;
+while (counter <= 333) {
+    if (counter %2 !== 0) {
+        console.log(counter)
+    }
+    counter +=1;
+}*/
+
+//17.4
+
+/*var  counter = 5;
+while (counter <= 50){
+    if (counter % 5 === 0 && counter % 3 === 0){
+        console.log(counter);
+    }
+    counter += 1;
+}*/
+
+//17.5
+
 
 
